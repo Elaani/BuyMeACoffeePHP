ddb45286e6f95708c9786efc1f251d40c5b79176
Sarah Duncan <forget95@gmail.com>
2023-08-26 11:04:24 -0400

    update sql to create items table

--- database.sql
+++ database.sql
@@ -12,10 +12,22 @@ CREATE TABLE payments (
     paymentId int(11) unsigned NOT NULL AUTO_INCREMENT,
     userId int(11) unsigned NOT NULL,
     paypalEmail varchar(100) DEFAULT NULL,
-    stripePublishableKey varchar(40) DEFAULT NULL,
-    stripePrivateKey varchar(40) DEFAULT NULL,
     currency char(3) NOT NULL DEFAULT 'USD',
     PRIMARY KEY (paymentId),
     UNIQUE KEY (userId),
+    FOREIGN KEY (userId) REFERENCES users(userId)
+) ENGINE=InnoDb CHARSET=utf8mb4;
+
+
+CREATE TABLE items (
+    itemId int(11) unsigned NOT NULL AUTO_INCREMENT,
+    userId int(11) unsigned NOT NULL,
+    idName varchar(50) NOT NULL,
+    itemName varchar(100) NOT NULL,
+    businessName varchar(100) DEFAULT NULL,
+    summary text DEFAULT NULL,
+    price decimal(8, 2) NOT NULL DEFAULT '0.00',
+    PRIMARY KEY (itemId),
+    UNIQUE KEY (idName),
     FOREIGN KEY (userId) REFERENCES users(userId)
 ) ENGINE=InnoDb CHARSET=utf8mb4;
